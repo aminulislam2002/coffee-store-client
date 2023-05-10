90935de9ca30a711e266700adad86589f0469454
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-10 10:10:35 -0700

    route setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import AddCoffee from "./components/AddCoffee";
+import UpdateCoffee from "./components/UpdateCoffee";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "addCoffee",
+    element: <AddCoffee></AddCoffee>,
+  },
+  {
+    path: "updateCoffee",
+    element: <UpdateCoffee></UpdateCoffee>,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
