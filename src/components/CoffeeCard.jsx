6c61dd64f2c85cf818ed6be5da036aa11b4c1f81
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-11 09:54:18 -0700

    delete a coffee and fetch coffee for udpate

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,5 +1,34 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
 const CoffeeCard = ({ coffee }) => {
-  const { name, quantity, supplier, taste, category, details, price, voucher, photoURL } = coffee;
+  const { _id, name, quantity, supplier, taste, category, details, price, voucher, photoURL } = coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your coffee has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div>
@@ -16,12 +45,17 @@ const CoffeeCard = ({ coffee }) => {
             <p>Price: {price} taka</p>
             <p>Quantity: {quantity}</p>
             <p>Voucher: {voucher}</p>
+            <p>Details: {details}</p>
           </div>
           <div>
             <div className="btn-group btn-group-vertical space-y-3">
               <button className="btn">View</button>
-              <button className="btn">Edit</button>
-              <button className="btn">Delete</button>
+              <Link to={`updateCoffee/${_id}`}>
+                <button className="btn">Edit</button>
+              </Link>
+              <button className="btn" onClick={() => handleDelete(_id)}>
+                Delete
+              </button>
             </div>
           </div>
         </div>
